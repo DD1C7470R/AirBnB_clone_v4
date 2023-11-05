8e51589713753b6244344d5dd47a706cba4dc448
DD1C74707 <gracevalley101@gmail.com>
2023-11-05 10:01:37 -0800

    Add task 5

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,68 @@
+$(function() {
+	$(document).ready(function() {
+		$("input[type='checkbox']").change(function() {
+			var amenitiesList = {};
+			amenitieIds = []
+			$("input[type='checkbox']:checked").each(function() {
+				var amenityId = $(this).data('id');
+				var amenityName = $(this).data('name');
+				amenitiesList[amenityId] = amenityName;
+				amenitieIds.push(dataId)
+			});
+
+			var amenitiesText = Object.values(amenitiesList).join(', ');
+			$(".amenities").find("h4").text(amenitiesText);
+		});
+		$.ajax({
+			url: 'http://0.0.0.0:5001/api/v1/status/',
+			type: 'GET',
+			success: function(response) {
+				if (response.status === 'OK') {
+					$('#api_status').addClass('available');
+				} else {
+					$('#api_status').removeClass('available');
+				}
+			},
+			error: function(error) {
+				console.error('Error:', error);
+				$('#api_status').removeClass('available');
+			}
+		});
+		$('button').click(function() {
+		$.ajax({
+			type: 'POST',
+			data: JSON.stringify({amenities: amenitieIds}),
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			url: 'http://0.0.0.0:5001/api/v1/places_search',
+			success: function(data) {
+				$.each(data, function(idx, place){
+					$('.places').append(
+						'<article>' +
+						  '<div class="title_box">' +
+						  '<h2>' + place.name + '</h2>' +
+						  '<div class="price_by_night">' + place.price_by_night +
+						  '</div>' +
+						  '</div>' +
+						  '<div class="information">' +
+						  '<div class="max_guest">' +
+						  '<br />' + place.max_guest + ' Guests' +
+						  '</div>' +
+						  '<div class="number_rooms">' +
+						  '<br />' + place.number_rooms + ' Bedrooms' +
+						  '</div>' +
+						  '<div class="number_bathrooms">' +
+						  '<br />' + place.number_bathrooms + ' Bathroom' +
+						  '</div>' +
+						  '</div>' +
+						  '<div class="description">' + place.description +
+						  '</div>' +
+						  '</article>');
+				});
+			}
+		})
+
+	})
+})
+})
